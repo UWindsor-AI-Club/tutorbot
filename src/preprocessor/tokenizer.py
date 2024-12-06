6e410a136a8a47bf88ad4b10454b055996acab41
kkatanaga <katanag@uwindsor.ca>
2024-12-06 01:53:22 -0500

    Added comments to classes and methods. Added short description for each Tokenizer constructor parameters

--- src/preprocessor/tokenizer.py
+++ src/preprocessor/tokenizer.py
@@ -3,29 +3,47 @@ import tiktoken
 import torch
 from torch.utils.data import Dataset, DataLoader
 
+# Class that tokenizes raw text.
+# The purpose is to convert a string of words (an incompatible format)
+#   to a vector format which are compatible with LLMs, specifically neural networks
+#
+# Two ways to tokenize text (for example, raw_string contains the entire text to tokenize):
+#   1. Pass in the raw text during initialization (i.e., Tokenizer(raw_string))
+#   2. Call tokenize(raw_string) after initialization
+# Both will return the token embedding
 class Tokenizer:
+    # (str) raw_text        : unmodified, raw string of text for the LLM to interpret
+    # (int) batch_size      : # of batches per tensor
+    # (int) max_length      : # of token ID's per batch
+    # (int) stride          : # of token ID positions shifted for a batch from its previous batch
+    # (int) output_dim      : dimension of the embedding vector (result)
+    # (str) encoding_model  : tokenizer model. Other options can be found at https://github.com/openai/tiktoken/blob/main/tiktoken/model.py
     def __init__(self, raw_text=None, batch_size=4, max_length=256,
-                    stride=128, output_dim=256):
+                    stride=128, output_dim=256, encoding_model='gpt-2'):
         self.batch_size = batch_size
         self.max_length = max_length
         self.stride     = stride
         self.output_dim = output_dim
 
         # Initialize the tokenizer
-        self.tokenizer = tiktoken.get_encoding("gpt2")
+        self.tokenizer = tiktoken.get_encoding(encoding_model)
         self.vocab_size = self.tokenizer.n_vocab
 
         if raw_text is not None:
             self.tokenize()
-        
+    
+    # Returns the input embedding. Returns None if raw_text was not entered during or after initialization
     def embeddings(self):
         return self.input_embeddings
     
+    # Tokenize a raw input text. Automatically called if the raw input text is passed into the constructor
+    # (str) raw_text    : unmodified, raw string of text for the LLM to interpret
     def tokenize(self, raw_text):
         self.data_loader = self._create_dataloader(raw_text, self.tokenizer, shuffle=False)
         self.input_embeddings = self._create_input_embeddings()
         return self.input_embeddings
 
+    # Helper function. Tokenizes the input and saves it as iterable dataset/batches
     def _create_dataloader(self, txt, tokenizer, shuffle=True, drop_last=True, num_workers=0):
         # Create dataset
         dataset = GPTDatasetV1(txt, tokenizer, self.max_length, self.stride)
@@ -39,6 +57,7 @@ class Tokenizer:
             num_workers=num_workers
         )
     
+    # Helper function. Converts tokenized batches to embeddings
     def _create_input_embeddings(self):
         data_iter = iter(self.data_loader)
         inputs, targets = next(data_iter)
@@ -53,6 +72,7 @@ class Tokenizer:
 
         return token_embeddings + pos_embeddings
 
+# Helper class. Used to encode raw text into token IDs
 class GPTDatasetV1(Dataset):
     def __init__(self, txt, tokenizer, max_length, stride):
         self.input_ids = []
